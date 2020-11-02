8ee007a33be9b50fdbe46a14251d85d728a0f2af
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-02 19:48:27 +0100

    refactor

--- lab1/zad1/src/prog.py
+++ lab1/zad1/src/prog.py
@@ -2,14 +2,10 @@ class Hamming:
     def distance(self, a, b):
         if a == "" and b == "":
             return 0
-        elif a == "A" and b == "A":
-            return 0
-        elif a == "G" and b == "T":
-            return 1
         elif len(a) > len(b):
             raise ValueError("First strand longer")
         elif len(b) > len(a):
-            raise  ValueError("Second strand longer")
+            raise ValueError("Second strand longer")
         else:
             wynik = 0
             for i in range(len(a)):
