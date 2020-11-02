d1d9444191efd959617616c5853bd3d9d968b9f2
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-02 19:15:02 +0100

    test2red

--- lab1/zad1/tests/tests.py
+++ lab1/zad1/tests/tests.py
@@ -4,8 +4,7 @@ import unittest
 class HammingTest(unittest.TestCase):
     def test_empty_strands(self):
         self.assertEqual(hamming.distance("", ""), 0)
-
-    @unittest.skip
+        
     def test_single_letter_identical_strands(self):
         self.assertEqual(hamming.distance("A", "A"), 0)
 
