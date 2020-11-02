86927219997066489bdc67c29c3826217a4daeea
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-02 20:49:36 +0100

    test8green

--- lab1/zad2/src/program.py
+++ lab1/zad2/src/program.py
@@ -1,3 +1,5 @@
+import math
+
 def roman(number):
     if number == 1:
         return "I"
@@ -13,3 +15,28 @@ def roman(number):
         return "VI"
     elif number == 9:
         return "IX"
+    else:
+        napis = ""
+        baseROM = ""
+        base = 1
+        while number != 0:
+            if number >= 10:
+                base = 10
+                baseROM = "X"
+            elif number >= 9:
+                base = 9
+                baseROM = "IX"
+            elif number >= 5:
+                base = 5
+                baseROM = "V"
+            elif number >= 4:
+                base = 4
+                baseROM = "IV"
+            elif number >= 1:
+                base = 1
+                baseROM = "I"
+
+            howmany = number // base
+            number = number - (howmany*base)
+            napis += howmany * baseROM
+        return napis
