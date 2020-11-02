2c94ac7bdac2c011fca757aef4c9b74c0d0f4898
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-02 21:10:54 +0100

    refactor

--- lab1/zad2/src/program.py
+++ lab1/zad2/src/program.py
@@ -1,21 +1,6 @@
 import math
 
 def roman(number):
-    if number == 1:
-        return "I"
-    elif number == 2:
-        return "II"
-    elif number == 3:
-        return "III"
-    elif number == 4:
-        return "IV"
-    elif number == 5:
-        return "V"
-    elif number == 6:
-        return "VI"
-    elif number == 9:
-        return "IX"
-    else:
         napis = ""
         baseROM = ""
         base = 1
