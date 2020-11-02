2b292ce3cd2a9466b4d03fc1856636e9aa756eac
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-03 00:10:40 +0100

    test12red

--- lab1/zad3/tests/testApp.py
+++ lab1/zad3/tests/testApp.py
@@ -58,7 +58,6 @@ class SongTest(unittest.TestCase):
                                         "seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling "
                                         "Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
-    @unittest.skip
     def test_one_verse_eleventh(self):
         self.assertEqual(song.sing(12), "On the twelfth day of Christmas my true love gave to me: twelve Drummers "
                                         "Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, "
