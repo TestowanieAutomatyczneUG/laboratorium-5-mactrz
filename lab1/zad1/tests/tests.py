5efac1e4456e4cd7d944069e975916744e32c54e
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-02 19:18:29 +0100

    test4red

--- lab1/zad1/tests/tests.py
+++ lab1/zad1/tests/tests.py
@@ -11,7 +11,6 @@ class HammingTest(unittest.TestCase):
     def test_single_letter_different_strands(self):
         self.assertEqual(hamming.distance("G", "T"), 1)
 
-    @unittest.skip
     def test_long_identical_strands(self):
         self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
 
